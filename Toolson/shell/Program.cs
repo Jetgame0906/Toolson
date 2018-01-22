a5f234a3ca4debd1a58b1c93f0d17b86a9637ecb
mkaraki <mkaraki_apps_publisher@outlook.jp>
2018-01-22 16:21:18 +0900

    First Project
    Add Main Project
    Add Shell Project

---
+++ Toolson/shell/Program.cs
@@ -0,0 +1,23 @@
+﻿//Assign at mkaraki
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Readcommand(string command)
+        {
+
+        }
+    }
+}
